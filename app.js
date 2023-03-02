92b197c527b514a77d4f40b8a64faddf056ca6aa
MinChan Lee <rlcks01537@gmail.com>
2023-03-02 16:29:50 +0900

    2023-03-02 painting-js-clone-coding commit

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
+const textInput = document.getElementById("text");
 const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 700;
-canvas.height = 700;
+const modeBtn = document.getElementById("mode-btn");
+const initializationBtn = document.getElementById("initialization-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const fileInput = document.getElementById("file");
+const saveBtn = document.getElementById("save");
+
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 700;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 // const colors = [
 //   "#ff3838",
@@ -15,7 +27,9 @@ ctx.lineWidth = lineWidth.value;
 //   "#32ff7e",
 //   "#7efff5",
 // ];
+
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPainting === true) {
@@ -44,9 +58,82 @@ function onColorChange(event) {
   ctx.fillStyle = event.target.value;
 }
 
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "채우기";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "그리기";
+  }
+}
+
+function onCanvasDown() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onInitializationClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "채우기";
+}
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
+
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== null) {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "50px Nunito";
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "fampla.png";
+  a.click();
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("mousedown", onCanvasDown);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+modeBtn.addEventListener("click", onModeClick);
+initializationBtn.addEventListener("click", onInitializationClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+canvas.addEventListener("dblclick", onDoubleClick);
+saveBtn.addEventListener("click", onSaveClick);
+
+colorOption.forEach((color) => color.addEventListener("click", onColorClick));
